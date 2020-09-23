f77bd861acc6cac8b53bf7ccfbac020fd00e47ba
phuodtr <phuoczsmm@gmail.com>
2020-09-24 00:03:31 +0700

    first commit

---
+++ AnimalSystem/IMarineAnimal.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AnimalSystem
+{
+    public interface IMarineAnimal : IAnimal
+    {
+        public void Move();
+    }
+}
